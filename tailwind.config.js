1b9c2ce83fcc114162ce8802b77f385b07eb6388
muhammaderlangga99 <muhammaderlangga159@hotmail.com>
2025-11-07 13:04:02 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = { // <-- UBAH DI SINI
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,mdx}",
+    "./docs/**/*.{md,mdx}",
+    "./blog/**/*.{md,mdx}",
+    "./docusaurus.config.ts",
+  ],
+  darkMode: ["class", "[data-theme='dark']"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    // Ini penting untuk menghindari konflik dengan style bawaan Docusaurus
+    preflight: false,
+  },
+};
